dd9b2f38434b6aacde2fe5508265e3e1357c56e0
IrinAnoVo <irinapavelviktor@gmail.com>
2025-05-08 11:09:36 +0200

    new

--- lecture_11_greedy_practice/lecture_11_greedy_practice/practice/backpack/Item.java
+++
@@ -1,8 +0,0 @@
-package org.telran.lecture_11_greedy_practice.practice.backpack;
-
-public class Item {
-    public String name;
-    public int weight;
-    public int value;
-    public int unit_value; // Удельная ценность - цена за единицу веса
-}
